4540a0d2aebca38170cfbace85ec188bad159cc5
Ramashankar Kumar <ramashankar.kumar@gridinfocom.com>
2022-08-30 10:06:51 +0530

    Second Commit

--- crud.go
+++ crud.go
@@ -14,7 +14,7 @@ import (
 func main() {
 	serverAPIOptions := options.ServerAPI(options.ServerAPIVersion1)
 	clientOptions := options.Client().
-		//	ApplyURI("mongodb+srv://ramashankar:mathematics@cluster0.qstjmc9.mongodb.net/?retryWrites=true&w=majority").
+		//	ApplyURI("mongodb+srv://ramashankar:<XXXXXXXXXXX>@cluster0.qstjmc9.mongodb.net/?retryWrites=true&w=majority").
 		ApplyURI("mongodb://localhost:27017").
 		SetServerAPIOptions(serverAPIOptions)
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
